65788f6a0f54a3ebaaa681a4b90495dcc80a7508
lbartron <LeoB909@hotmail.com>
2025-03-05 15:31:18 -0800

    Co-authored-by: Caeden117 <caeden.s@outlook.com> Co-authored-by: icarlee <icarlee@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-print("hello world!")
-print("carlee :)")
-print("Rasajna :)")
-print("hi there hello hi")
+# Importing libraries
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib as mpl
+
+# py -3.11 pip install 
+
+# Read our stocks data into a DataFrame object
+#  Specify delimiter to ',' to match CSV format
+#  Skip errors in our CSV file because I do not want to look through that file for every little error :)
+# date,open,high,low,close,volume,Name
+data = pd.read_csv('stocks_5yrs.csv', delimiter=',', on_bad_lines='skip') 
